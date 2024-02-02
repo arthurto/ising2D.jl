379b206d25f52ecf9c017c794064c9d6e50fe1df
arthurto <arthur.ps22009@gmail.com>
2024-02-02 03:03:45 -0300

    fixed issues

--- src/base.jl
+++ src/base.jl
@@ -11,17 +11,17 @@ function sum(sites::Vector{Site})::Int
     return sum(get_value.(sites))
 end
 
-abstract type Lattice <: Matrix{Site} end
+# mutable struct Lattice{Matrix{Site}} end
 
-function lattice(N::Int)::Lattice 
+function lattice(N::Int)::Matrix{Site}#::Lattice 
     return [Site(rand(Bool)) for _ in 1:N, _ in 1:N]
 end
 
 function index(i::Int64,nc::Int64)::Int64
-    return mod(i,nr)+1
+    return mod(i,nc)+1
 end 
 
-function sum_neighbors(lattice::Lattice,i::Int,j::Int)::Int8 
+function sum_neighbors(lattice::Matrix{Site},i::Int,j::Int)::Int8 
     nr,nc = size(lattice)
     # Com certeza deve ter um jeito mais elegante de escrever isso 
     # Mas essa foi a melhor forma que eu encontrei de somar os vizinhos respeitando 
@@ -35,17 +35,17 @@ function sum_neighbors(lattice::Lattice,i::Int,j::Int)::Int8
     return s
 end
 
-function sum_neighbors(J::Real,lattice::Lattice,i::Int,j::Int)::Int8
+function sum_neighbors(J::Real,lattice::Matrix{Site},i::Int,j::Int)::Int8
     # Summing the neighbors of i,j
     return -J*get_value(lattice[index(i),index(j)])*sum_neighbors(lattice,i,j)
 end
 
-function sum_neighbors(J::Real,lattice::Lattice,i::Int,j::Int,flip=true)::Int8
+function sum_neighbors(J::Real,lattice::Matrix{Site},i::Int,j::Int,flip=true)::Int8
     # Summing the neighbors of i,j, with i,j flipped
     return -J*get_value(!lattice[index(i),index(j)])*sum_neighbors(lattice,i,j)
 end
 
-function sum_neighbors(J::Real,lattice::Latttice)::Real
+function sum_neighbors(J::Real,lattice::Matrix{Site})::Real
     # Total energy
     nr,nc = size(lattice)
     S = Real(0)
@@ -55,7 +55,7 @@ function sum_neighbors(J::Real,lattice::Latttice)::Real
     return S
 end
 
-function sum_neighbors(J::Real,lattice::Latttice,flip=true)::Real
+function sum_neighbors(J::Real,lattice::Matrix{Site},flip=true)::Real
     # Total energy for flipped
     nr,nc = size(lattice)
     S = Real(0)
@@ -65,7 +65,7 @@ function sum_neighbors(J::Real,lattice::Latttice,flip=true)::Real
     return S
 end
 
-function flip!(lattice::Lattice,i::Int,j::Int)
+function flip!(lattice::Matrix{Site},i::Int,j::Int)
     # Flipping a spin in the i,j position,
     # or, more generally, changing the current state 
     # to some other (in this case, only one) 
@@ -76,7 +76,7 @@ function flip!(lattice::Lattice,i::Int,j::Int)
     lattice[index(i),index(j)] = !lattice[index(i),index(j)]
 end
 
-function evolve_state!(J::Real,β::Real,lattice::Lattice,i::Int,j::Int,E_old::Float64)
+function evolve_state!(J::Real,β::Real,lattice::Matrix{Site},i::Int,j::Int,E_old::Float64)
     E_new = sum_neighbors(J,lattice,i,j,true)
     
     # A variação de energia 
